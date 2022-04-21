28f9439b66dbe45363c20a28347ce6cd27b046e9
Hadi Kutabi <hadi.kutabi@tu-dortmund.de>
2022-04-21 14:29:52 +0200

    updated

--- luigi_examples/wipe_pipeline_data.py
+++ luigi_examples/wipe_pipeline_data.py
@@ -1,10 +1,8 @@
 import os
 
 directory = ("data/")
-
 files_in_directory = os.listdir(directory)
-
-filtered_files = [file for file in files_in_directory if (file.endswith(".json") or file.endswith("pkl"))]
+filtered_files = [file for file in files_in_directory if file != "taxy_trips_ny_2016-06-01to03_3%sample.csv"]
 
 for file in filtered_files:
     path_to_file = os.path.join(directory, file)
