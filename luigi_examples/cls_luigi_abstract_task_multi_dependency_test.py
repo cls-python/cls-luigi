5febaa0d8d24567157ef9e0a6944592e68b1eb95
Hadi Kutabi <hadi.kutabi@tu-dortmund.de>
2022-05-25 15:35:41 +0200

    extended the test with dummy task .Removed UniqueTaskPipelineValidator. Added

--- luigi_examples/cls_luigi_abstract_task_multi_dependency_test.py
+++ luigi_examples/cls_luigi_abstract_task_multi_dependency_test.py
@@ -1,39 +1,8 @@
-from collections.abc import Iterable
-
 import luigi
 from inhabitation_task import LuigiCombinator, ClsParameter, RepoMeta
 from cls_python import FiniteCombinatoryLogic, Subtypes
+from unique_task_pipeline_validator import UniqueTaskPipelineValidator
 
-
-class UniqueTaskPipelineValidator(object):
-
-    def __init__(self, unique_abstract_classes):
-        self.unique_abstract_classes = unique_abstract_classes
-
-    def validate(self, pipeline):
-        traversal = self.dfs(pipeline)
-        concrete_cls_map = dict()
-        for obj in traversal:
-            for cls in self.unique_abstract_classes:
-                if isinstance(obj, cls):
-                    if cls in concrete_cls_map:
-                        concrete_cls = concrete_cls_map[cls]
-                        if not isinstance(obj, concrete_cls):
-                            return False
-                    else:
-                        concrete_cls_map[cls] = obj.__class__
-        return True
-
-    def dfs(self, start):
-        traversal = [start]
-        dependencies = start.requires()
-        if isinstance(dependencies, Iterable):
-            for dependency in start.requires():
-                traversal.extend(self.dfs(dependency))
-        else:
-            traversal.extend(self.dfs(dependencies))
-
-        return traversal
 
 class Task1(luigi.Task, LuigiCombinator):
     abstract = False
@@ -133,10 +102,9 @@ class Task5(luigi.Task, LuigiCombinator):
 class Task6(luigi.Task, LuigiCombinator):
     abstract = False
     task5 = ClsParameter(tpe=Task5.return_type())
-    task2 = ClsParameter(tpe= Task2Abstract.return_type())
 
     def requires(self):
-        return [self.task5(), self.task2()]
+        return [self.task5()]
 
     def run(self):
         print("Now Task6")
@@ -147,8 +115,121 @@ class Task6(luigi.Task, LuigiCombinator):
         return luigi.LocalTarget("data/Task6.txt")
 
 
-class EndNode(luigi.WrapperTask, LuigiCombinator):
+class Task7Abstract(luigi.Task, LuigiCombinator):
+    abstract = True
     task6 = ClsParameter(tpe=Task6.return_type())
+
+    def requires(self):
+        return self.task6()
+
+
+class Task7A(Task7Abstract):
+    abstract = False
+
+    def run(self):
+        print("Now Task7A")
+        with open(self.output().path, "w+") as outfile:
+            outfile.write("Task7A")
+
+    def output(self):
+        return luigi.LocalTarget("data/Task7A.txt")
+
+
+class Task7B(Task7Abstract):
+    abstract = False
+
+    def run(self):
+        print("Now Task7B")
+        with open(self.output().path, "w+") as outfile:
+            outfile.write("Task7B")
+
+    def output(self):
+        return luigi.LocalTarget("data/Task7B.txt")
+
+
+class Dummy1(luigi.Task, LuigiCombinator):
+    abstract = False
+    task7 = ClsParameter(tpe=Task7Abstract.return_type())
+
+    def requires(self):
+        return [self.task7()]
+
+    def run(self):
+        print("Now Dummy1")
+        with open(self.output().path, "w+") as outfile:
+            outfile.write("Dummy1")
+
+    def output(self):
+        return luigi.LocalTarget("data/Dummy1.txt")
+
+
+class Dummy2(luigi.Task, LuigiCombinator):
+    abstract = False
+    task7 = ClsParameter(tpe=Task7Abstract.return_type())
+
+    def requires(self):
+        return [self.task7()]
+
+    def run(self):
+        print("Now Dummy2")
+        with open(self.output().path, "w+") as outfile:
+            outfile.write("Dummy2")
+
+    def output(self):
+        return luigi.LocalTarget("data/Dummy2.txt")
+
+
+class Dummy(Dummy1, Dummy2):
+    abstract = False
+    task7 = ClsParameter(tpe=Task7Abstract.return_type())
+
+    def requires(self):
+        return [self.task7()]
+
+    def run(self):
+        print("Now Dummy2")
+        with open(self.output().path, "w+") as outfile:
+            outfile.write("Dummy2")
+
+    def output(self):
+        return luigi.LocalTarget("data/Dummy2.txt")
+
+
+s = {Dummy1, Dummy2}
+
+
+class DummyWrapper(luigi.WrapperTask, LuigiCombinator):
+    abstract = False
+
+    def requires(self):
+        return [self.dummy1(), self.dummy2()]
+
+    def output(self):
+        return self.input()
+
+
+class Task8(luigi.Task, LuigiCombinator):
+    abstract = False
+    task2 = ClsParameter(tpe=Task2Abstract.return_type())
+    task7 = ClsParameter(tpe=Task7Abstract.return_type())
+    dummy1 = ClsParameter(tpe=Dummy1.return_type() if Dummy1 in s else Dummy.return_type())
+    dummy2 = ClsParameter(tpe=Dummy2.return_type() if Dummy2 in s else Dummy.return_type())
+
+    def requires(self):
+        return [self.task2(), self.task7(), self.dummy1(), self.dummy2()]
+        # return luigi.task.flatten([self.task2(), self.task7()])
+
+    def run(self):
+        print("Now Task8")
+        with open(self.output().path, "w+") as outfile:
+            outfile.write("Task8")
+
+    def output(self):
+        return luigi.LocalTarget("data/Task8.txt")
+
+
+class EndNode(luigi.WrapperTask, LuigiCombinator):
+    task6 = ClsParameter(tpe=Task8.return_type())
 
     def requires(self):
         return self.task6()
@@ -169,11 +250,12 @@ if __name__ == '__main__':
     max_results = max_tasks_when_infinite
     if actual > 0:
         max_results = actual
-    validator = UniqueTaskPipelineValidator([Task2Abstract])
+    validator = UniqueTaskPipelineValidator([Task2Abstract, Task7Abstract])
     results = [t() for t in inhabitation_result.evaluated[0:max_results] if validator.validate(t())]
 
     if results:
         print("Number of results", max_results)
+        print("Number of results after filtering", len(results))
         print("Run Pipelines")
         luigi.build(results, local_scheduler=False, detailed_summary=True)
     else:
