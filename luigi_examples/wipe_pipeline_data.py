2fbfe26368f8ed0a65c628264e0c4033a34efb11
Hadi Kutabi <hadi.kutabi@tu-dortmund.de>
2022-06-30 15:36:31 +0200

    added data in hellow_world_examples

--- luigi_examples/wipe_pipeline_data.py
+++ luigi_examples/wipe_pipeline_data.py
@@ -7,3 +7,10 @@ filtered_files = [file for file in files_in_directory if file != "taxy_trips_ny_
 for file in filtered_files:
     path_to_file = os.path.join(directory, file)
     os.remove(path_to_file)
+
+
+hello_world_path = "../hello_world_examples"
+for file in os.listdir(hello_world_path):
+    if not file.endswith(".py") and not file.endswith(".jpg"):
+        if not os.path.isdir(os.path.join(hello_world_path, file)):
+            os.remove(os.path.join(hello_world_path, file))
