5335d611fa05bd8a6f43a2a5fbd6e2e4ea878f93
Hadi Kutabi <hadi.kutabi@tu-dortmund.de>
2022-08-29 10:38:34 +0200

    added async functionality for fetching repo; removed run.sh

--- repo_visualizer/app.js
+++ repo_visualizer/app.js
@@ -1,39 +1,61 @@
-async function fetchJSONRepo(path = "repo.json") {
-  const response = await fetch(path);
-
-  return response.json();
-};
-
-const repo = await fetchRepo();
-
-
+async function Draw() {
 
   // Set up zoom support
-var svg = d3.select("svg"),
+  var svg = d3.select("svg"),
   inner = svg.append("g"),
   zoom = d3.zoom().on("zoom", function() {
     inner.attr("transform", d3.event.transform);
   });
   svg.call(zoom);
 
-var render = new dagreD3.render();
+  var render = new dagreD3.render();
 
   // Left-to-right layout
-var g = new dagreD3.graphlib.Graph()
+  var g = new dagreD3.graphlib.Graph()
   .setGraph({
-  nodesep: 100,
-  ranksep: 70,
-  rankdir: "LR"
-});
-    
-function addNode(component){
-  var componentDetails = repo[component];
-    var html = "<div>";
-    var className = "notAbstractComponent";
+    nodesep: 100,
+    ranksep: 70,
+    rankdir: "LR"
+  });
+
+async function fetchJSONRepo(path = "repo.json"){
+  const fetched = await fetch(path);
+
+  return fetched.json();
+}
+
+const repo = await fetchJSONRepo();
+
+async function getTotalNumberOfPipelines(r){
+  await r;
+
+  let n = 1;
+  for (var component in r){
+    if (r[component]["abstract"]){
+      n = n * r[component]["concreteImplementations"].length;
+    }
+  }
+  return n;
+}
+
+d3.select("total-pipelines")
+  .append("div")
+  .classed("total-pipelines", true)
+  .append("div")
+  .classed("title", true)
+  .text("Total Number of Pipelines: " + await getTotalNumberOfPipelines(repo));
+
+async function AddNodesAndEdges(r, withLable=false) {
+  await r;
+  for (let component in r) {
+    let componentDetails = r[component];
+
+     // Add Node
+    let html = "<div>";
+    let className = "notAbstractComponent";
     html += "<span class=status></span>";
     html += "<span class=name>"+component+"</span>";
     html += "<span class=queue>" + ""+"</span>";
-
 
     if (componentDetails.concreteImplementations){
       className = "abstractComponent"
@@ -52,65 +74,28 @@ function addNode(component){
       padding: 0,
       class: className,
     });
-};
 
-function addEdge(component, withLable= true) {
-  var componentDetails = repo[component];
-  if (componentDetails.inputQueue){
-    for (const d of componentDetails.inputQueue){
-      var edgeLable = NaN;
-      if (repo[d]["abstract"]){
-        edgeLable = repo[d]["concreteImplementations"].length.toString();
-      }
-      if (withLable){
-        g.setEdge(d, component, {label: edgeLable});
-      } else {
+    // Add Edge
+    if (componentDetails.inputQueue){
+      for (const d of componentDetails.inputQueue){
         g.setEdge(d, component, {});
       }
     }
   }
-};
+}
+await AddNodesAndEdges(repo, withLable=true);
 
-function draw(isUpdate) {
-  for (var component in repo) {
-    addNode(component);
-    addEdge(component, withLable=false);
-  }
-  inner.call(render, g);
-  
-  // Zoom and scale to fit
-  var graphWidth = g.graph().width + 80;
-  var graphHeight = g.graph().height + 40;
-  var width = parseInt(svg.style("width").replace(/px/, ""));
-  var height = parseInt(svg.style("height").replace(/px/, ""));
-  var zoomScale = Math.min(width / graphWidth, height / graphHeight);
-  var translateX = (width / 2) - ((graphWidth * zoomScale) / 2) + 25;
-  var translateY = (height / 2) - ((graphHeight * zoomScale) / 2);
-  var svgZoom = isUpdate ? svg.transition().duration(500) : svg;
-  svgZoom.call(zoom.transform, d3.zoomIdentity.translate(translateX, translateY).scale(zoomScale));
+inner.call(render, g);
+
+// Zoom and scale to fit
+var graphWidth =  g.graph().width + 80;
+var graphHeight =  g.graph().height + 40;
+var width =  parseInt(svg.style("width").replace(/px/, ""));
+var height =  parseInt(svg.style("height").replace(/px/, ""));
+var zoomScale =  Math.min(width / graphWidth, height / graphHeight);
+var translateX =  (width / 2) - ((graphWidth * zoomScale) / 2) + 25;
+var translateY =  (height / 2) - ((graphHeight * zoomScale) / 2);
+svg.call(zoom.transform, d3.zoomIdentity.translate(translateX, translateY).scale(zoomScale));
 }
 
-
-function getTotalNumberOfPipelines(){
-  var n = 1;
-  for (var component in repo){
-    if (repo[component]["abstract"] === true){
-      n = n * repo[component]["concreteImplementations"].length;
-    }
-  }
-  return n;
-}
-
-
-d3.select("total-pipelines")
-  .append("div")
-  .classed("total-piplelines", true)
-  .append("div")
-  .classed("title", true)
-  .text("Total Number of Pipelines: " + getTotalNumberOfPipelines());
-
-
-
-
-
-document.addEventListener("DOMContentLoaded", draw);
+document.addEventListener("DOMContentLoaded", Draw);
