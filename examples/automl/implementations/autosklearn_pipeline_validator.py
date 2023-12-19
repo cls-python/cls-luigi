2e4959ef7174f600dfb42f52bf2f75df180e426c
Hadi <hadi.kutabi@kit.edu>
2023-12-19 10:10:42 +0100

    using strings in forbidden

--- examples/automl/implementations/autosklearn_pipeline_validator.py
+++ examples/automl/implementations/autosklearn_pipeline_validator.py
@@ -1,47 +1,33 @@
-from typing import List
+from typing import List, Set
 
 import luigi
 from luigi.task import flatten
 
-from cls_luigi.inhabitation_task import RepoMeta
+from .forbidden import FORBIDDEN
 
 
 class AutoMLPipelineValidator(object):
-    def __init__(self, forbidden_tasks: List[List[RepoMeta]]) -> None:
+    def __init__(self, forbidden_tasks: List[Set[str]] = FORBIDDEN) -> None:
         self.forbidden_tasks = forbidden_tasks
-        self.map_forbidden_tasks_to_family_name()
 
     def validate(self, pipeline: luigi.Task) -> bool:
         pipeline = self.get_pipeline_as_list(pipeline)
         return self._check_subset(pipeline, self.forbidden_tasks)
 
-    def map_forbidden_tasks_to_family_name(self) -> None:
-        for ix, collection in enumerate(self.forbidden_tasks):
-            for ix_2, task in enumerate(collection):
-                if isinstance(task, RepoMeta):
-                    self.forbidden_tasks[ix][ix_2] = task.task_family
-                elif isinstance(task, str):
-                    self.forbidden_tasks[ix][ix_2] = task
-
     @staticmethod
-    def _check_subset(pipeline: List[str], subsets: List[str]) -> bool:
-        set_pipeline = set(pipeline)
+    def _check_subset(pipeline: List[str], subsets: Set[str]) -> bool:
+        pipeline = set(pipeline)
         for s in subsets:
-            subset = set(s)
-            if subset.issubset(set_pipeline) is True:
+            if s.issubset(pipeline) is True:
                 return False
         return True
 
-    def get_pipeline_as_list(self, task: luigi.Task, task_list: List = None) -> List[str]:
-        if task_list is None:
-            task_list = []
+    def get_pipeline_as_list(self, task: luigi.Task) -> List[str]:
+        pipeline = [task.task_family]
+        children = flatten(task.requires())
 
-        task_name = task.task_family
-
-        if task_name not in task_list:
-            task_list.append(task_name)
-        dependencies = flatten(task.requires())
-        for dep in dependencies:
-            task_list = self.get_pipeline_as_list(dep, task_list)
-
-        return task_list
+        if children:
+            for child in children:
+                if child.task_family not in pipeline:
+                    pipeline.extend(self.get_pipeline_as_list(child))
+        return pipeline
--- examples/automl/implementations/forbidden.py
+++ examples/automl/implementations/forbidden.py
@@ -1,70 +1,44 @@
-from .classifiers.knn import SKLKNearestNeighbors
-from .classifiers.gaussian_nb import SKLGaussianNaiveBayes
-from .classifiers.multinominal_nb import SKLMultinomialNB
-from .classifiers.svc import SKLKernelSVC
-from .classifiers.decision_tree import SKLDecisionTree
-from .classifiers.adaboost import SKLAdaBoost
-from .classifiers.gradient_boosting import SKLGradientBoosting
-from .classifiers.extra_trees import SKLExtraTrees
-from .classifiers.random_forest import SKLRandomForest
-from .classifiers.qda import SKLQuadraticDiscriminantAnalysis
-from .classifiers.lda import SKLLinearDiscriminantAnalysis
-
-from .feature_preprocessors.fast_ica import SKLFastICA
-from .feature_preprocessors.kernel_pca import SKLKernelPCA
-from .feature_preprocessors.nystroem import SKLNystroem
-from .feature_preprocessors.pca import SKLPCA
-from .feature_preprocessors.polynomial_features import SKLPolynomialFeatures
-from .feature_preprocessors.random_trees_embedding import SKLRandomTreesEmbedding
-from .feature_preprocessors.rbf_sampler import SKLRBFSampler
-from .feature_preprocessors.select_percentile import SKLSelectPercentile
-from .feature_preprocessors.select_rates import SKLSelectRates
-from .feature_preprocessors.select_from_extra_trees_clf import SKLSelectFromExtraTrees
-from .feature_preprocessors.select_from_svc_clf import SKLSelectFromLinearSVC
-from .feature_preprocessors.feature_agglomeration import SKLFeatureAgglomeration
-from .feature_preprocessors.no_feature_preprocessor import NoFeaturePreprocessor
-
 
 
 FORBIDDEN = [
-    [SKLMultinomialNB, SKLNystroem],
-    [SKLMultinomialNB, SKLKernelPCA],
-    [SKLMultinomialNB, SKLFastICA],
-    [SKLMultinomialNB, SKLPCA],
-    [SKLMultinomialNB, SKLRBFSampler],
-    [SKLGaussianNaiveBayes, SKLNystroem],
-    [SKLGaussianNaiveBayes, SKLRBFSampler],
-    [SKLGaussianNaiveBayes, SKLKernelPCA],
-    [SKLRandomForest, SKLNystroem],
-    [SKLRandomForest, SKLRBFSampler],
-    [SKLRandomForest, SKLKernelPCA],
-    [SKLKernelSVC, SKLNystroem],
-    [SKLKernelSVC, SKLRBFSampler],
-    [SKLKernelSVC, SKLKernelPCA],
-    [SKLKNearestNeighbors, SKLNystroem],
-    [SKLKNearestNeighbors, SKLRBFSampler],
-    [SKLKNearestNeighbors, SKLKernelPCA],
-    [SKLGradientBoosting, SKLNystroem],
-    [SKLGradientBoosting, SKLRBFSampler],
-    [SKLGradientBoosting, SKLKernelPCA],
-    [SKLExtraTrees, SKLNystroem],
-    [SKLExtraTrees, SKLRBFSampler],
-    [SKLExtraTrees, SKLKernelPCA],
-    [SKLDecisionTree, SKLNystroem],
-    [SKLDecisionTree, SKLRBFSampler],
-    [SKLDecisionTree, SKLKernelPCA],
-    [SKLAdaBoost, SKLNystroem],
-    [SKLAdaBoost, SKLRBFSampler],
-    [SKLAdaBoost, SKLKernelPCA],
-    [SKLMultinomialNB, SKLSelectRates],
-    [SKLMultinomialNB, SKLSelectPercentile],
-    [SKLQuadraticDiscriminantAnalysis, SKLRandomTreesEmbedding],
-    [SKLLinearDiscriminantAnalysis, SKLRandomTreesEmbedding],
-    [SKLGradientBoosting, SKLRandomTreesEmbedding],
-    [SKLGaussianNaiveBayes, SKLRandomTreesEmbedding],
-    [SKLMultinomialNB, SKLPolynomialFeatures],
-    [SKLMultinomialNB, NoFeaturePreprocessor],
-    [SKLMultinomialNB, SKLSelectFromLinearSVC],
-    [SKLMultinomialNB, SKLFeatureAgglomeration],
-    [SKLMultinomialNB, SKLSelectFromExtraTrees],
+    {'SKLMultinomialNB', 'SKLNystroem'},
+    {'SKLMultinomialNB', 'SKLKernelPCA'},
+    {'SKLMultinomialNB', 'SKLFastICA'},
+    {'SKLMultinomialNB', 'SKLPCA'},
+    {'SKLMultinomialNB', 'SKLRBFSampler'},
+    {'SKLGaussianNaiveBayes', 'SKLNystroem'},
+    {'SKLGaussianNaiveBayes', 'SKLRBFSampler'},
+    {'SKLGaussianNaiveBayes', 'SKLKernelPCA'},
+    {'SKLRandomForest', 'SKLNystroem'},
+    {'SKLRandomForest', 'SKLRBFSampler'},
+    {'SKLRandomForest', 'SKLKernelPCA'},
+    {'SKLKernelSVC', 'SKLNystroem'},
+    {'SKLKernelSVC', 'SKLRBFSampler'},
+    {'SKLKernelSVC', 'SKLKernelPCA'},
+    {'SKLKNearestNeighbors', 'SKLNystroem'},
+    {'SKLKNearestNeighbors', 'SKLRBFSampler'},
+    {'SKLKNearestNeighbors', 'SKLKernelPCA'},
+    {'SKLGradientBoosting', 'SKLNystroem'},
+    {'SKLGradientBoosting', 'SKLRBFSampler'},
+    {'SKLGradientBoosting', 'SKLKernelPCA'},
+    {'SKLExtraTrees', 'SKLNystroem'},
+    {'SKLExtraTrees', 'SKLRBFSampler'},
+    {'SKLExtraTrees', 'SKLKernelPCA'},
+    {'SKLDecisionTree', 'SKLNystroem'},
+    {'SKLDecisionTree', 'SKLRBFSampler'},
+    {'SKLDecisionTree', 'SKLKernelPCA'},
+    {'SKLAdaBoost', 'SKLNystroem'},
+    {'SKLAdaBoost', 'SKLRBFSampler'},
+    {'SKLAdaBoost', 'SKLKernelPCA'},
+    {'SKLMultinomialNB', 'SKLSelectRates'},
+    {'SKLMultinomialNB', 'SKLSelectPercentile'},
+    {'SKLQuadraticDiscriminantAnalysis', 'SKLRandomTreesEmbedding'},
+    {'SKLLinearDiscriminantAnalysis', 'SKLRandomTreesEmbedding'},
+    {'SKLGradientBoosting', 'SKLRandomTreesEmbedding'},
+    {'SKLGaussianNaiveBayes', 'SKLRandomTreesEmbedding'},
+    {'SKLMultinomialNB', 'SKLPolynomialFeatures'},
+    {'SKLMultinomialNB', 'NoFeaturePreprocessor'},
+    {'SKLMultinomialNB', 'SKLSelectFromLinearSVC'},
+    {'SKLMultinomialNB', 'SKLFeatureAgglomeration'},
+    {'SKLMultinomialNB', 'SKLSelectFromExtraTrees'}
 ]
