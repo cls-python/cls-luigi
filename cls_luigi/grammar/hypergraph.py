3dc9a34bed28f0548d0eabba39bcf818f8859495
Hadi Kutabi <hadi.kutabi@kit.edu>
2024-07-31 10:57:13 +0200

    renamed types and subtypes to terminals and non terminals

--- cls_luigi/grammar/hypergraph.py
+++ cls_luigi/grammar/hypergraph.py
@@ -2,7 +2,9 @@ from typing import Dict, List, Tuple
 
 import networkx as nx
 import matplotlib.pyplot as plt
+from matplotlib import patches
 from matplotlib.lines import Line2D
+from matplotlib.patches import Patch
 
 
 def get_hypergraph_dict_from_tree_grammar(
@@ -12,23 +14,23 @@ def get_hypergraph_dict_from_tree_grammar(
     hypergraph = {
         "choice_edges": [],
         "arg_edges": [],
-        "type_nodes": [],
-        "subtype_nodes": []}
+        "non_terminal_nodes": [],
+        "terminal_nodes": []}
 
     for lhs, rhs in tree_grammar.items():
 
-        if lhs not in hypergraph["type_nodes"]:
-            hypergraph["type_nodes"].append(lhs)
+        if lhs not in hypergraph["non_terminal_nodes"]:
+            hypergraph["non_terminal_nodes"].append(lhs)
 
         for combinator, args in rhs.items():
 
-            if combinator not in hypergraph["subtype_nodes"]:
-                hypergraph["subtype_nodes"].append(combinator)
+            if combinator not in hypergraph["terminal_nodes"]:
+                hypergraph["terminal_nodes"].append(combinator)
 
             hypergraph["choice_edges"].append((lhs, combinator))
             for arg in args:
-                if arg not in hypergraph["type_nodes"]:
-                    hypergraph["type_nodes"].append(arg)
+                if arg not in hypergraph["non_terminal_nodes"]:
+                    hypergraph["non_terminal_nodes"].append(arg)
 
                 hypergraph["arg_edges"].append((combinator, arg))
 
@@ -43,8 +45,8 @@ def plot_hypergraph_components(
     node_size: int = 5000,
     figsize: Tuple[int, int] = (13, 8),
     facecolor: str = "White",
-    type_nodes_color: str = "#003049",
-    subtype_nodes_color: str = "#b87012",
+    non_terminal_nodes_color: str = "#003049",
+    terminal_nodes_color: str = "#b87012",
     choice_edges_color: str = "#818589",
     arg_edges_color: str = "black",
     choice_edges_style: str = "dotted",
@@ -62,18 +64,18 @@ def plot_hypergraph_components(
     fig.patch.set_facecolor(facecolor)
 
     all_edges = hypergraph["choice_edges"] + hypergraph["arg_edges"]
-    all_nodes = hypergraph["type_nodes"] + hypergraph["subtype_nodes"]
+    all_nodes = hypergraph["non_terminal_nodes"] + hypergraph["terminal_nodes"]
 
     g = nx.DiGraph()
     g.add_nodes_from(all_nodes)
     g.add_edges_from(all_edges)
     pos = nx.bfs_layout(g, start=start_node)
 
-    nx.draw_networkx_nodes(g, pos, node_size=node_size, nodelist=hypergraph["type_nodes"],
-                           ax=axs, node_color=type_nodes_color, node_shape='s')
+    nx.draw_networkx_nodes(g, pos, node_size=node_size, nodelist=hypergraph["non_terminal_nodes"],
+                           ax=axs, node_color=non_terminal_nodes_color, node_shape='s')
 
-    nx.draw_networkx_nodes(g, pos, node_size=node_size, nodelist=hypergraph["subtype_nodes"],
-                           ax=axs, node_color=subtype_nodes_color, node_shape='o')
+    nx.draw_networkx_nodes(g, pos, node_size=node_size, nodelist=hypergraph["terminal_nodes"],
+                           ax=axs, node_color=terminal_nodes_color, node_shape='o')
 
     nx.draw_networkx_edges(g, pos, ax=axs, edge_color=choice_edges_color, edgelist=hypergraph["choice_edges"],
                            arrowstyle=arrow_style, arrowsize=arrows_size, width=arrow_width, style=choice_edges_style,
@@ -89,8 +91,11 @@ def plot_hypergraph_components(
 
     legend_elements = [
         Line2D([0], [0], color=choice_edges_color, lw=2, linestyle=choice_edges_style, label='Choice'),
-        Line2D([0], [0], color=arg_edges_color, lw=2, label='Arg', linestyle=arg_edges_style)
+        Line2D([0], [0], color=arg_edges_color, lw=2, label='Arg', linestyle=arg_edges_style),
+        Patch(facecolor=non_terminal_nodes_color, edgecolor=non_terminal_nodes_color, label="Non-terminals"),
+        Line2D([0], [0], marker='o', color=terminal_nodes_color, label='Terminals', markerfacecolor=terminal_nodes_color, markersize=20),
     ]
+
     axs.legend(handles=legend_elements, loc=legend_loc)
 
     plt.tight_layout()
