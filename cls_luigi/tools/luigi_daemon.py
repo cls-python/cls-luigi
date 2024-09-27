d23b1ccbfe3e9a7cdd49e777b9228b767f2b5cdc
Hadi Kutabi <hadi.kutabi@kit.edu>
2024-09-27 15:38:48 +0200

    added luigi_daemon.py to tools

---
+++ cls_luigi/tools/luigi_daemon.py
@@ -0,0 +1,37 @@
+import logging
+import subprocess
+from time import sleep
+from typing import Optional
+
+
+class LinuxLuigiDaemonHandler:
+
+    def __init__(
+        self,
+        logdir: str = "/tmp/",
+        logger: Optional[logging.Logger] = None
+    ) -> None:
+
+        self.logdir = logdir
+        self.luigi_process = None
+        if logger:
+            self.logger = logger
+        else:
+            self.logger = logging.getLogger(self.__class__.__name__)
+
+    def __enter__(self):
+        try:
+            self.luigi_process = subprocess.Popen(["luigid", "--background", "--logdir", self.logdir])
+            self.luigi_process.wait()
+            sleep(0.5)  # just waiting till system stabilizes
+        except Exception as e:
+            self.logger.warning("Could not kill Luigi daemon: {e}")
+            raise e
+
+    def __exit__(self, *args):
+        try:
+            self.luigi_process = subprocess.Popen(["pkill", "-f", "luigid"])
+            self.luigi_process.wait()
+            sleep(0.5)  # just waiting till system stabilizes
+        except Exception as e:
+            raise e
