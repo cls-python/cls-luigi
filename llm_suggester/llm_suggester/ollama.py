ab70d6927e5b96718dfe0cacddf5395f8fe09fc7
anikinil <anikins.ilja@gmail.com>
2025-07-18 21:24:20 +0200

    tried to fix the message logging

--- llm_suggester/llm_suggester/ollama.py
+++ llm_suggester/llm_suggester/ollama.py
@@ -1,15 +1,10 @@
 from ollama import chat
 import json
 
-# MODEL_NAME = "deepseek-r1:1.5b"
-# MODEL_NAME = "deepseek-r1:7b"
 MODEL_NAME = "deepseek-r1:8b"
-# MODEL_NAME = "qwen3:8b"
-# MODEL_NAME = "qwen3:14b"
 
 
-
-class DirectGrammarAgent:
+class GrammarAgent:
 
     def __init__(self, task, grammar, path):
         
@@ -21,10 +16,36 @@ class DirectGrammarAgent:
         self.history_file_path = path + "/grammar_agent_history.txt"
         self.grammar_file_path = path + "/llm_reduced_grammar.json"
         
+    def generate_grammar(self):
+        raise NotImplementedError("This method should be implemented by subclasses.")
+    
+    def save_grammar(self, new_grammar):
+        with open(self.grammar_file_path, "w") as f:
+            json.dump(json.loads(new_grammar), f, indent=4)
+        print("Grammar saved to", self.grammar_file_path)
+            
+    def log_message(self, message):
+        with open(self.history_file_path, "a") as f:
+            f.write("------------------------------------------\n")
+            f.write(str(message.role) + ":\n\n")
+            for part in message.parts:
+                if part.content != None: f.write(str(part.content) + "\n")
+                if part.function_call != None: f.write("> Function call: " + str(part.function_call) + "\n")
+                if part.function_response != None: f.write("> Function response: " + str(part.function_response) + "\n")
+            f.write("\n")
+
+
+class DirectGrammarAgent(GrammarAgent):
+
+    def __init__(self, task, grammar, path):
+
+        super().__init__(task, grammar, path)
+        
         self.instructions = f"""You are a helpful and rational agent, and your goal is to create a pipeline for the following machine learning task: "{self.task}".\n
 The following is a regular tree grammar, which defines the pipeline tasks and the rules for combining them, to build all possible pipelines for the above-mentioned task.\n{self.grammar}\n
 Your goal is to reduce the grammar, so that it produces only one valid pipeline, which you regard as the most well-suited for the task and the dataset. To suggest the modified grammar you should use the "suggest_grammar" tool.
-All your choices should be well thought out, so do not hesitate to explain your thinking process in the response.""" 
+"""
+# All your choices should be well thought out, so do not hesitate to explain your thinking process in the response.""" 
 
         self.messages = [
             {
@@ -65,16 +86,18 @@ All your choices should be well thought out, so do not hesitate to explain your 
         print("Generating LLM response...")
         
         response = ""
-        # TODO find out, how to make deepseek-r1 think
-        for part in chat(model=self.model, messages=self.messages, tools=self.tools, think=False, stream=True):
+        for part in chat(model=self.model, messages=self.messages, tools=self.tools, stream=True):
+            if part.message.tool_calls:
+                print("Tool call detected:", part.message.tool_calls)
             content = part.message.content
             print(content, end='', flush=True)
             response += content
-        
-        # response = chat(model=self.model, messages=self.messages, tools=self.tools) # non-streaming version
-        self.log_message(response.message)
-        
-        tool_call = response.message.tool_calls[0] if response.message.tool_calls else None
+
+        print(json.loads("{\"message\": {\"parts\": [{\"content\": \"" + str(response) + "\"}]}, \"role\": \"assistant\"}"))
+
+        self.log_message(json.loads({"message": {"parts": [{"content": str(response)}]}, "role": "assistant"}))
+
+        tool_call = part.message.tool_calls[0] if part.message.tool_calls else None
         if tool_call == 'suggest_grammar':
             suggested_grammar = tool_call.arguments.get('grammar', None)
             self.save_grammar(suggested_grammar)
@@ -84,18 +107,3 @@ All your choices should be well thought out, so do not hesitate to explain your 
             print("No tool call in the response.")
             return None
     
-    def save_grammar(self, new_grammar):
-        with open(self.grammar_file_path, "w") as f:
-            json.dump(json.loads(new_grammar), f, indent=4)
-        print("Grammar saved to", self.grammar_file_path)
-            
-    def log_message(self, message):
-        with open(self.history_file_path, "a") as f:
-            f.write("------------------------------------------\n")
-            f.write(str(message.role) + ":\n\n")
-            for part in message.parts:
-                if part.content != None: f.write(str(part.content) + "\n")
-                if part.function_call != None: f.write("> Function call: " + str(part.function_call) + "\n")
-                if part.function_response != None: f.write("> Function response: " + str(part.function_response) + "\n")
-            f.write("\n")
-
